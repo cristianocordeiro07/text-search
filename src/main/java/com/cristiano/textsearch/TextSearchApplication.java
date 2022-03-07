ebcaf5532a2300d2ac98a49f6fab3527daa2c6ac
cristianocordeiro07 <cristiano.cordeiro07@gmail.com>
2022-03-07 14:19:19 -0300

    Refactoring

--- src/main/java/com/cristiano/textsearch/TextSearchApplication.java
+++ src/main/java/com/cristiano/textsearch/TextSearchApplication.java
@@ -23,39 +23,39 @@ public class TextSearchApplication {
         SpringApplication.run(TextSearchApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner init(BookRepository bookRepository) {
-        return args -> {
-            Stream.of("In Search of Lost Time", "Ulysses", "Don Quixote").forEach(title -> {
-                Book book = new Book(title, "Cristiano");
-                bookRepository.save(book);
-            });
-            bookRepository.findAll().forEach(book ->
-                    System.out.println(book.getId() + " - " + book.getTitle() + " - " + book.getAuthor()));
-        };
-    }
-
-    @Bean
-    CommandLineRunner init2(MagazineRepository magazineRepository) {
-        return args -> {
-            Stream.of("Veja", "IstoE").forEach(name -> {
-                Magazine magazine = new Magazine(name, new Date(Calendar.getInstance().getTime().getTime()));
-                magazineRepository.save(magazine);
-            });
-            magazineRepository.findAll().forEach(magazine ->
-                    System.out.println(magazine.getId() + " - " + magazine.getName() + " - " + magazine.getPublicationDate()));
-        };
-    }
-
-    @Bean
-    CommandLineRunner init3(NotebookRepository notebookRepository) {
-        return args -> {
-            Stream.of("Cristiano").forEach(owner -> {
-                Notebook notebook = new Notebook(owner);
-                notebookRepository.save(notebook);
-            });
-            notebookRepository.findAll().forEach(notebook ->
-                    System.out.println(notebook.getId() + " - " + notebook.getOwner()));
-        };
-    }
+//    @Bean
+//    CommandLineRunner init(BookRepository bookRepository) {
+//        return args -> {
+//            Stream.of("In Search of Lost Time", "Ulysses", "Don Quixote").forEach(title -> {
+//                Book book = new Book(title, "Cristiano");
+//                bookRepository.save(book);
+//            });
+//            bookRepository.findAll().forEach(book ->
+//                    System.out.println(book.getId() + " - " + book.getTitle() + " - " + book.getAuthor()));
+//        };
+//    }
+//
+//    @Bean
+//    CommandLineRunner init2(MagazineRepository magazineRepository) {
+//        return args -> {
+//            Stream.of("Veja", "IstoE").forEach(name -> {
+//                Magazine magazine = new Magazine(name, new Date(Calendar.getInstance().getTime().getTime()));
+//                magazineRepository.save(magazine);
+//            });
+//            magazineRepository.findAll().forEach(magazine ->
+//                    System.out.println(magazine.getId() + " - " + magazine.getName() + " - " + magazine.getPublicationDate()));
+//        };
+//    }
+//
+//    @Bean
+//    CommandLineRunner init3(NotebookRepository notebookRepository) {
+//        return args -> {
+//            Stream.of("Cristiano").forEach(owner -> {
+//                Notebook notebook = new Notebook(owner);
+//                notebookRepository.save(notebook);
+//            });
+//            notebookRepository.findAll().forEach(notebook ->
+//                    System.out.println(notebook.getId() + " - " + notebook.getOwner()));
+//        };
+//    }
 }
